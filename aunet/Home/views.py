899ed76d32e3a6d51cc8182312c8dda13a029c8c
lyjdwh <1412511544@qq.com>
2016-11-11 22:56:57 +0800

    fix some bugs

--- aunet/Home/views.py
+++ aunet/Home/views.py
@@ -7,7 +7,7 @@ import json
 
 
 from . import home
-from .models import News,Category, Tag, SilderShow
+from .models import News,Category, Tag, SilderShow,news_category
 
 @home.route('/',methods=["POST","GET"])
 @home.route('/index',methods=["POST","GET"])
@@ -27,9 +27,10 @@ def index():
 					,LatestAdvanceNotice=LatestAdvanceNotice,NewsPinPai=NewsPinPai,\
 					NewsCharmHust=NewsCharmHust,NewsYuLan=NewsYuLan)
 
-@home.route('/news/<int:year>/<int:month>/<int:day>',methods=["POST","GET"])
-def show_News(year,month,day):
-	return render_template("Home/news/news.html")
+@home.route('/news/<int:id>',methods=["POST","GET"])
+def show_News(id):
+	news=News.query.filter(News.id==id).first()
+	return render_template("Home/news/index.html",news=news)
 
 @home.route('/news',methods=["POST","GET"])
 def indexNews():
@@ -49,60 +50,60 @@ def getNews(number,category):
 
 def news2Json(news,length,page):
 	newsJson=dict()
-	newsJson['news_Title']=list()
-	newsJson['news_Outline']=list()
-	newsJson['news_Img_Url']=list()
-	newsJson['news_Post_Time']=list()
-	newsJson['news_Length']=length
-	newsJson['news_Current_Page']=str(page)
+	newsJson['title']=list()
+	newsJson['outline']=list()
+	newsJson['img_url']=list()
+	newsJson['post_time']=list()
+	newsJson['length']=length
+	newsJson['current_page']=str(page)
 	i=0
 	for new in news:
-		newsJson['news_Title'].append(dict({i:new.title}))
-		newsJson['news_Outline'].append(dict({i:new.outline}))
-		newsJson['news_Img_Url'].append(dict({i:new.img_url}))
-		newsJson['news_Post_Time'].append(dict({i:new.post_time.strftime('%Y-%m-%d %H:%M:%S')}))
+		newsJson['title'].append(dict({i:new.title}))
+		newsJson['outline'].append(dict({i:new.outline}))
+		newsJson['img_url'].append(dict({i:new.img_url}))
+		newsJson['post_time'].append(dict({i:new.post_time.strftime('%Y-%m-%d %H:%M:%S')}))
 		i=i+1
 	return newsJson   		
 	
 @home.route('/news/news2Json',methods=["POST","GET"])
 def newJson():
 	if request.method == 'POST':
-		getDict=request.get_json()				
+		get_dict=request.get_json()				
 		now = datetime.now()
-		Category=getDict['Category']
-		Time=getDict['Time']
-		Sort=getDict['Sort']
-		gotoPage=getDict['gotoPage']
-		gotoPage=int(gotoPage)
+		category=get_dict['Category']
+		time=get_dict['Time']
+		sort=get_dict['Sort']
+		goto_page=get_dict['gotoPage']
+		goto_page=int(goto_page)
 		# Category=request.values.get['Category']
 		# Time=request.values.get['Time']
 		# Sort=request.values.get['Sort']
 		# gotoPage=request.values.get['gotoPage']
-		if Time=="all":
-			Time=now-timedelta(days=365*10)
-		elif Time=="week":
-			Time=now -timedelta(days=7)
-		elif Time=="month":
-			Time=now -timedelta(days=90)
-		elif Time=="year":
-			Time=now-timedelta(days=365)
+		if time=="all":
+			time=now-timedelta(days=365*10)
+		elif time=="week":
+			time=now -timedelta(days=7)
+		elif time=="month":
+			time=now -timedelta(days=90)
+		elif time=="year":
+			time=now-timedelta(days=365)
 
-		if(Category=="all" and (Sort=="all" or Sort=="hot" or Sort=="latest")):
-			news=News.query.filter(News.post_time>Time).order_by(News.post_time.desc()).all()
-			news=news[(gotoPage-1)*10:gotoPage*10]
-		elif(Category=="all" and Sort=="oldest"):
-			news=News.query.filter(News.post_time>Time).order_by(News.post_time).all()
-			news=news[(gotoPage-1)*10:gotoPage*10]
-		elif(Category!="all" and (Sort=="all" or Sort=="hot" or Sort=="latest")):
-			news=News.query.filter(News.post_time>Time,News.cate==category).order_by(News.post_time.desc()).all()
-			news=news[(gotoPage-1)*10:gotoPage*10]
-		elif(Category!="all" and Sort=="oldest"):
-			news=News.query.filter(News.post_time>Time,News.cate==category).order_by(News.post_time).all()
-			news=news[(gotoPage-1)*10:gotoPage*10]
+		if(category=="all" and (sort=="all" or sort=="hot" or sort=="latest")):
+			news=News.query.filter(News.post_time>time).order_by(News.post_time.desc()).all()
+			news=news[(goto_page-1)*10:goto_page*10]
+		elif(category=="all" and sort=="oldest"):
+			news=News.query.filter(News.post_time>time).order_by(News.post_time).all()
+			news=news[(goto_page-1)*10:goto_page*10]
+		elif(category!="all" and (sort=="all" or sort=="hot" or sort=="latest")):
+			news=News.query.join(news_category).join(Category).filter(News.post_time>time).filter(Category.name==category).order_by(News.post_time.desc()).all()
+			news=news[(goto_page-1)*10:goto_page*10]
+		elif(category!="all" and sort=="oldest"):
+			news=News.query.join(news_category).join(Category).filter(News.post_time>time,Category.name==category).order_by(News.post_time).all()
+			news=news[(goto_page-1)*10:goto_page*10]
 		else:
 			news=None
 		if news!=None:
-			NewsJson=news2Json(news,len(news),gotoPage)
+			NewsJson=news2Json(news,len(news),goto_page)
 			return jsonify(NewsJson)
 		else:
 			return "<html><body>bad</body></html>"
