f9ac65675d28809fc4a487fa1a77bf95171c2ff5
caibingcheng <1193376579@qq.com>
2016-11-29 19:49:05 +0800

    rewrite some styles

--- aunet/__init__.py
+++ aunet/__init__.py
@@ -1,13 +1,12 @@
 #-*-coding:utf-8-*-
 # 导入各扩展
-from flask import Flask,session
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_restful import Api,Resource
 from flask_login import LoginManager
 from flask_principal import Principal
 import pymysql
-from datetime import timedelta
 
 
 # from .Api.common.common import errors
@@ -27,11 +26,6 @@ principals=Principal(app)
 lm=LoginManager()
 lm.init_app(app)
 lm.login_view='getApp'
-
-#设置flask的session和cookit的过期时间
-app.permanent_session_lifetime = timedelta(hours=6)
-lm.remember_cookie_duration=timedelta(hours=6)
-
 
 mail=Mail(app)
 
