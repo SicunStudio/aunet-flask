3c49f5695d19a4a688bfe931110b77c65f8b4a82
lyjdwh <1412511544@qq.com>
2016-11-30 09:38:58 +0800

    improve manange.py function

--- manage.py
+++ manage.py
@@ -3,7 +3,8 @@ from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
 
 from aunet import app,db
-
+from aunet.Home.models import News,news_category,news_tag,Category,Tag,	SilderShow
+from aunet.Admin.models import User,Node,Role,user_role,role_node
 
 
 migrate=Migrate(app,db)
@@ -15,13 +16,14 @@ manager.add_command("db",MigrateCommand)#数据库我迁移命令
 @manager.option('-n', '--name', dest="name",help='Your name',default="admin")
 @manager.option('-p','--password',dest='password',help="Your password",default="123456")
 @manager.option('-e','--email',dest="email",help="your email",default=None)
-def CreateSuperUser(name,password,email):
+@manager.option('--phone',dest="phone",help="your phone",default=None)
+def CreateSuperUser(name,password,email,phone):
 	try:
-		user=User(name,password,email)
+		user=User(name,password,email,phone)
 		user.addRole("超管")
 		db.session.add(user)
 		db.session.commit()
-		print ("successfully create a super user name:%s,password:%s,email:%s"%(name,password,email))
+		print ("successfully create a super user name:%s,password:%s,email:%s,phone:%s"%(name,password,email,phone))
 	except:
 		print("something wrong.please ensure you have created a super role and your name is unique")
 
@@ -47,6 +49,12 @@ def CreateSuperRole():
 		node16=Node("修改节点",1)
 		node17=Node("materialAdmin",1)
 		node18=Node("materialAction",1)
+		node19=Node("查看新闻",1)
+		node20=Node("查看新闻栏目",1)
+		node21=Node("查看新闻标签",1)
+		node22=Node("查看用户",1)
+		node23=Node("查看角色",1)
+		node24=Node("查看权限节点",1)
 		db.session.add(node1)
 		db.session.add(node2)
 		db.session.add(node3)
@@ -65,6 +73,12 @@ def CreateSuperRole():
 		db.session.add(node16)
 		db.session.add(node17)
 		db.session.add(node18)
+		db.session.add(node19)
+		db.session.add(node20)
+		db.session.add(node21)
+		db.session.add(node22)
+		db.session.add(node23)
+		db.session.add(node24)
 		db.session.add(role)
 		db.session.commit()
 		role=Role.query.filter(Role.roleName=="超管").first()
@@ -86,14 +100,20 @@ def CreateSuperRole():
 		role.addNode("修改节点")
 		role.addNode("materialAdmin")
 		role.addNode("materialAction")
+		role.addNode("查看新闻")
+		role.addNode("查看新闻栏目")
+		role.addNode("查看新闻标签")
+		role.addNode("查看用户")
+		role.addNode("查看角色")
+		role.addNode("查看权限节点")
+
 		db.session.add(role)
 		db.session.commit()
 		print("successfully create a super role name:超管");
 	except:
 		print("you can only run it once")
 
-from aunet.Home.models import News,news_category,news_tag,Category,Tag,	SilderShow
-from aunet.Admin.models import User,Node,Role,user_role,role_node
+
 
 if __name__ == '__main__':
     manager.run()
