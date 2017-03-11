ccaa998568fa7ba4249677f39daf10e5ac801d5d
lyjdwh <1412511544@qq.com>
2017-03-11 14:11:23 +0800

    refactor in Admin and Home and add comments and modify README.md

--- aunet/Admin/models.py
+++ aunet/Admin/models.py
@@ -2,137 +2,139 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from flask_principal import Permission
-from .. import lm,db
+
 
 from datetime import datetime
 from collections import namedtuple
 from functools import partial
 
+from .. import lm, db
 
-EditUserNeed=partial(namedtuple("user",['method','value']),'edit')
+EditUserNeed = partial(namedtuple("user", ['method', 'value']), 'edit')
+
+
 class EditUserPermission(Permission):
-    def __init__(self,user_id):
-        need=EditUserNeed(id)
-        super(EditUserPermission,self).__init__(need)
+
+    def __init__(self, user_id):
+        need = EditUserNeed(id)
+        super(EditUserPermission, self).__init__(need)
 
 
-role_node = db.Table('role_node',  # 角色权限关联表  
-    db.Column('node_id', db.Integer, db.ForeignKey('node.id')),
-    db.Column('role_id', db.Integer, db.ForeignKey('role.id')),
-    db.Column('created_at', db.DateTime, default=datetime.now)
-    )
+role_node = db.Table('role_node',  # 角色权限关联表
+                     db.Column(
+                         'node_id', db.Integer, db.ForeignKey('node.id')),
+                     db.Column(
+                         'role_id', db.Integer, db.ForeignKey('role.id')),
+                     db.Column('created_at', db.DateTime, default=datetime.now)
+                     )
 
-user_role=	db.Table('user_role',	#用户角色关联表
-	db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('role_id', db.Integer, db.ForeignKey('role.id')),
-    db.Column('created_at', db.DateTime, default=datetime.now)
-	)
-
-
-
-class User(db.Model,UserMixin):
-	__tablename__="user"
-	id = db.Column(db.Integer, primary_key=True)
-	userName = db.Column(db.String(64), unique=True, index=True)
-	passWord =db.Column(db.String(128))
-	email=db.Column(db.String(40))
-	status=db.Column(db.Boolean)
-	remark=db.Column(db.String(20))
-	phone=db.Column(db.String(20))
-	roles=db.relationship("Role",secondary=user_role,backref=db.backref('users',lazy="dynamic"))
+user_role = db.Table('user_role',  # 用户角色关联表
+                     db.Column(
+                         'user_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column(
+                         'role_id', db.Integer, db.ForeignKey('role.id')),
+                     db.Column('created_at', db.DateTime, default=datetime.now)
+                     )
 
 
-	def verify_password(self, passWord):
-		return check_password_hash(self.passWord,passWord)
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(64), unique=True, index=True)
+    passWord = db.Column(db.String(128))
+    email = db.Column(db.String(40))
+    status = db.Column(db.Boolean)
+    remark = db.Column(db.String(20))
+    phone = db.Column(db.String(20))
+    roles = db.relationship(
+        "Role", secondary=user_role, backref=db.backref('users', lazy="dynamic"))
 
-	def get_id(self):
-		return self.id
-  	
+    def verify_password(self, passWord):
+        return check_password_hash(self.passWord, passWord)
+
+    def get_id(self):
+        return self.id
+
     # def get_id(self):
     #     return str(self.id)  # python 3
 
-	@property
-	def nodes(self):
-		node=[]
-		for r in self.role:
-			node=node+r.nodes
-		return node
+    @property
+    def nodes(self):
+        node = []
+        for r in self.role:
+            node = node+r.nodes
+        return node
 
-	def addRole(self,roleName):
-		r=Role.query.filter(Role.roleName==roleName).first()
-		self.roles.append(r)
+    def addRole(self, roleName):
+        r = Role.query.filter(Role.roleName == roleName).first()
+        self.roles.append(r)
 
-    
-	def __init__(self,userName,passWord,email,phone):
-		self.userName = userName
-		self.passWord = generate_password_hash(passWord)
-		self.email=email
-		self.status=True
-		self.phone=phone
+    def __init__(self, userName, passWord, email, phone):
+        self.userName = userName
+        self.passWord = generate_password_hash(passWord)
+        self.email = email
+        self.status = True
+        self.phone = phone
 
-    	#self.role=Role.query.filter(Role.roleName==roleName).first()
+    def __str__(self):
+        return self.userName
 
-	def __str__(self):
-   		return self.userName
-
-	__repr__ = __str__
-
-
+    __repr__ = __str__
 
 
 class LoginLog(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	userName= db.Column(db.String(64))
-	loginTime=db.Column(db.DateTime)
-	loginIp=db.Column(db.String(40))
+    id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(64))
+    loginTime = db.Column(db.DateTime)
+    loginIp = db.Column(db.String(40))
 
-	def __init__(self,userName,loginIp):
-		self.userName=userName
-		self.loginTime=datetime.utcnow()
-		self.loginIp=loginIp
-	def __str__(self):
-		return self.userName
-	__repr__ = __str__
+    def __init__(self, userName, loginIp):
+        self.userName = userName
+        self.loginTime = datetime.utcnow()
+        self.loginIp = loginIp
+
+    def __str__(self):
+        return self.userName
+    __repr__ = __str__
+
 
 class Role(db.Model):
-	__tablename__="role"
-	id = db.Column(db.Integer, primary_key=True)
-	roleName=db.Column(db.String(30),unique=True)
-	status=db.Column(db.Boolean)
-	remark=db.Column(db.String(30))
-	nodes=db.relationship("Node",secondary=role_node,backref=db.backref('roles',lazy="dynamic"))
+    __tablename__ = "role"
+    id = db.Column(db.Integer, primary_key=True)
+    roleName = db.Column(db.String(30), unique=True)
+    status = db.Column(db.Boolean)
+    remark = db.Column(db.String(30))
+    nodes = db.relationship(
+        "Node", secondary=role_node, backref=db.backref('roles', lazy="dynamic"))
 
-	def addNode(self,nodeName):
-		n=Node.query.filter(Node.nodeName==nodeName).first()
-		self.nodes.append(n)
-		
+    def addNode(self, nodeName):
+        n = Node.query.filter(Node.nodeName == nodeName).first()
+        self.nodes.append(n)
 
-	def __init__(self,roleName):
-		self.roleName=roleName
-		self.status=1
+    def __init__(self, roleName):
+        self.roleName = roleName
+        self.status = 1
 
-	def __str__(self):
-		return self.roleName
+    def __str__(self):
+        return self.roleName
 
-	__repr__ = __str__
+    __repr__ = __str__
 
 
 class Node(db.Model):
-	__tablename__="node"
-	id = db.Column(db.Integer, primary_key=True)
-	nodeName=db.Column(db.String(30),unique=True)
-	remark=db.Column(db.String(30))	
-	status=db.Column(db.Boolean)
-	level=db.Column(db.Integer)
+    __tablename__ = "node"
+    id = db.Column(db.Integer, primary_key=True)
+    nodeName = db.Column(db.String(30), unique=True)
+    remark = db.Column(db.String(30))
+    status = db.Column(db.Boolean)
+    level = db.Column(db.Integer)
 
+    def __init__(self, nodeName, level):
+        self.nodeName = nodeName
+        self.status = 1
+        self.level = level
 
-	def __init__(self,nodeName,level):
-		self.nodeName=nodeName
-		self.status=1
-		self.level=level
+    def __str__(self):
+        return self.nodeName
 
-	def __str__(self):
-		return self.nodeName
-
-	__repr__ = __str__
-
+    __repr__ = __str__
--- aunet/Admin/views.py
+++ aunet/Admin/views.py
@@ -1,51 +1,59 @@
 # -*-coding:utf-8 -*-
-from flask import render_template,current_app,redirect,request,make_response,session
-from flask_login import current_user,login_user,logout_user,login_required
-from flask_principal import identity_loaded,RoleNeed,UserNeed,ActionNeed
+
+# import extensions and python library
+from flask import render_template, current_app, redirect, request, make_response, session
+from flask_login import current_user, login_user, logout_user, login_required
+from flask_principal import identity_loaded, RoleNeed, UserNeed, ActionNeed
 from flask_principal import Identity, AnonymousIdentity, \
-     identity_changed,Permission
-# from flask_restful import abort
-import json,os,random
+    identity_changed, Permission
+import json
+import os
+import random
 from werkzeug.security import generate_password_hash
 from flask import abort
-from .news import SilderShow1,SliderShowSpec,News1,NewsSpec,NewsSpecDetail,Tags,Tag1,Categorys,Category1
-from .users import Users,UserSpec,Roles,RoleSpec,Nodes,NodeSpec,CurrentUser
-from .search import SearchNews
-from .login import Login
-
-
-from aunet import lm,app,api,db
-from .models import User,LoginLog
-from .models import EditUserPermission,EditUserNeed
-from .email import send_email
-from . import admin
-from . import ts
 from collections import namedtuple
 from functools import partial
+from itsdangerous import URLSafeTimedSerializer
 
-basedir=app.config['BASEDIR']
-#user has the permission of edit himself
-EditUserNeed=partial(namedtuple("user",['method','value']),'edit')
+
+# import models
+from .news import SilderShowClass, SliderShowSpec, NewsClass, NewsSpec, NewsSpecDetail, Tags, TagClass, Categorys, CategoryClass
+from .users import Users, UserSpec, Roles, RoleSpec, Nodes, NodeSpec, CurrentUser
+from .search import SearchNews
+from .login import Login
+from .models import User, LoginLog
+from .models import EditUserPermission, EditUserNeed
+from .email import send_email
+
+from . import admin
+from . import ts
+from aunet import lm, app, api, db
+
+
+basedir = app.config['BASEDIR']
+# user has the permission of edit himself
+EditUserNeed = partial(namedtuple("user", ['method', 'value']), 'edit')
+
 
 class EditUserPermission(Permission):
-    def __init__(self,user_id):
-        need=EditUserNeed(int(user_id))
-        super(EditUserPermission,self).__init__(need)
+
+    def __init__(self, user_id):
+        need = EditUserNeed(int(user_id))
+        super(EditUserPermission, self).__init__(need)
 
 
+# load user to enable flask_login extension
 @lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
-# @lm.unauthorized_handler
-# def unauthorized_handle():
-#     return "good"
 
+# flask-principal load user's permission into session
 @identity_loaded.connect_via(app)
 def on_identity_loaded(sender, identity):
     # Set the identity user object
     identity.user = current_user
-    #user has the permission of edit himself
+    # user has the permission of edit himself
 
     identity.provides.add(EditUserPermission(current_user.id))
 
@@ -54,22 +62,18 @@ def on_identity_loaded(sender, identity):
         identity.provides.add(UserNeed(current_user.id))
         for role in current_user.roles:
             identity.provides.add(RoleNeed(role.roleName))
-        
+
     # Assuming the User model has a list of nodes, update the
     # identity with the nodes that the user provides
-    if hasattr(current_user,"roles"):
+    if hasattr(current_user, "roles"):
         for role in current_user.roles:
             for node in role.nodes:
-                if (node.status==1) and (current_user.status==1) and (role.status==1):
+                if (node.status == 1) and (current_user.status == 1) and (role.status == 1):
                     identity.provides.add(ActionNeed(node.nodeName))
-    	
-    
-'''   		
-@admin.route('/index')
-@admin.route('/')
-def index():    
-    return render_template("Admin/index.html")
 
+
+# login page. but now abandoned! maybe someday will use it
+'''         
 
 @admin.route('/login',methods=["POST","GET"])
 def login():
@@ -108,10 +112,9 @@ def login():
     error=None 
     return render_template("Admin/login.html",confirmCode=confirmCode,error=error)
 
-# @lm.unauthorized_handler
-# def unauthorized_handle():
-#     return "good"
 '''
+
+
 @admin.route('/logout')
 # @login_required
 def logout():
@@ -123,94 +126,78 @@ def logout():
         session.pop(key, None)
     return redirect(request.args.get('next') or '/')
 
-@app.route("/templates/Admin/<string:path>",methods=['GET',"POST"])
+
+@app.route("/templates/Admin/<string:path>", methods=['GET', "POST"])
 def getHtml(path):
-    # path=request.args.get("path","templates/Home/index/index.html")
-    #return "dg"
-    # path=os.path.join('/home/lyjdwh/Documents/aunet-flask','aunet/templates/Admin/',path)
-    path=os.path.join(basedir,'aunet/templates/Admin/',path)
-    # path=os.path.join('aunet/templates/Admin/',path)
-    path=str(path)
+    path = os.path.join(basedir, 'aunet/templates/Admin/', path)
     try:
-        f=open(path,'r', encoding='utf-8')
-        # text=f.read()
-        # res = make_response(text)
-        # return res
-        return f.read()
+        with open(path, 'r', encoding='utf-8') as f:
+            return f.read()
     except:
-        return "not found" ,404
+        return "not found", 404
 
 
 @app.route("/dashboard", methods=["GET"])
 @app.route("/dashboard/<path:path>", methods=["GET"])
 def getApp(path=None):
-    path=os.path.join(basedir,'aunet/templates/Admin/app.html')
-    with open(path,'r', encoding='utf-8') as f:
+    path = os.path.join(basedir, 'aunet/templates/Admin/app.html')
+    with open(path, 'r', encoding='utf-8') as f:
         return f.read()
 
 
+# 忘记密码功能还未实现
 
-
-    
-
-@admin.route('/upload',methods=['GET',"POST"])
-def upload():
-    pass
-
-#忘记密码功能还未实现
-@admin.route('/forget',methods=["GET","POST"])
+@admin.route('/forget', methods=["GET", "POST"])
 def forget():
-    if request.method=="POST":
-        userName=request.form['userName']
-        email=request.form['email']
-        user=User.query.filter(User.userName==userName).first()
-        subject="验证邮箱"
-        token=ts.dumps(user.userName,salt="you will never guess")
-        confirm_url=url_for("admin.confirm",token=token,_external=True)
-        html=render_template(
+    if request.method == "POST":
+        userName = request.form['userName']
+        email = request.form['email']
+        user = User.query.filter(User.userName == userName).first()
+        subject = "验证邮箱"
+        token = ts.dumps(user.userName, salt="you will never guess")
+        confirm_url = url_for("admin.confirm", token=token, _external=True)
+        html = render_template(
             'Admin/conform.html',
             confirm_url=confirm_url)
-        if email==user.email:
-            send_email(subject,user.email,html)
+        if email == user.email:
+            send_email(subject, user.email, html)
         return redirect(url_for("index"))
     return render_template("")
+
 
 @admin.route('/confirm/<token>')
 def confirm_email(token):
     try:
-        userName=ts.loads(token,salt="you will never guess", max_age=86400)
+        userName = ts.loads(token, salt="you will never guess", max_age=86400)
     except:
         abort(404)
-    user=User.query.filter(User.userName==userName).first_or_404()
-    user.passWord=generate_password_hash("123456")
+    user = User.query.filter(User.userName == userName).first_or_404()
+    user.passWord = generate_password_hash("123456")
     db.session.add(user)
     db.session.commit()
     return "good"
 
-#User 模块
-api.add_resource(CurrentUser,"/api/User/CurrentUser")
+# User 模块
+api.add_resource(CurrentUser, "/api/User/CurrentUser")
 api.add_resource(Users, '/api/User/Users')
-api.add_resource(UserSpec,"/api/User/Users/<string:id>")
-api.add_resource(Nodes,"/api/User/Nodes")
-api.add_resource(NodeSpec,"/api/User/Nodes/<string:id>")
-api.add_resource(Roles,"/api/User/Roles")
-api.add_resource(RoleSpec,"/api/User/Roles/<string:id>")
+api.add_resource(UserSpec, "/api/User/Users/<string:id>")
+api.add_resource(Nodes, "/api/User/Nodes")
+api.add_resource(NodeSpec, "/api/User/Nodes/<string:id>")
+api.add_resource(Roles, "/api/User/Roles")
+api.add_resource(RoleSpec, "/api/User/Roles/<string:id>")
 
 
-#News 模块
+# News 模块
+api.add_resource(SilderShowClass, "/api/News/SliderShow")
+api.add_resource(SliderShowSpec, "/api/News/SliderShow/<string:id>")
+api.add_resource(NewsClass, "/api/News/News")
+api.add_resource(NewsSpec, "/api/News/News/<string:id>")  # gai
+api.add_resource(NewsSpecDetail, "/api/News/News/<string:id>/Detail")
+api.add_resource(Tags, "/api/News/Tags")
+api.add_resource(TagClass, "/api/News/Tags/<string:id>")
+api.add_resource(Categorys, "/api/News/Categorys")
+api.add_resource(CategoryClass, "/api/News/Categorys/<string:id>")
 
-api.add_resource(SilderShow1,"/api/News/SliderShow")
-api.add_resource(SliderShowSpec,"/api/News/SliderShow/<string:id>")
-api.add_resource(News1,"/api/News/News")
-api.add_resource(NewsSpec,"/api/News/News/<string:id>")                 #gai
-api.add_resource(NewsSpecDetail,"/api/News/News/<string:id>/Detail")
-api.add_resource(Tags,"/api/News/Tags")
-api.add_resource(Tag1,"/api/News/Tags/<string:id>")
-api.add_resource(Categorys,"/api/News/Categorys")
-api.add_resource(Category1,"/api/News/Categorys/<string:id>")
-
-#Search 模块
-
-api.add_resource(SearchNews,"/api/Search/News")
-
-api.add_resource(Login,"/api/Login")
+# Search 模块
+api.add_resource(SearchNews, "/api/Search/News")
+api.add_resource(Login, "/api/Login")
